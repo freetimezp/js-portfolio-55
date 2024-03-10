a1722a26f33dc33b4393fd7486c91df144dc8fb1
freetimezp <kraftukrnet@ukr.net>
2024-03-10 14:36:44 +0200

    11. js-portfolio-55. Contact page.

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiMessengerLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
+
 import styles from './contact.module.css';
+import SectionHeader from '../components/section-header/SectionHeader';
 
 const page = () => {
     return (
-        <div>
-            contact
-        </div>
+        <section className={styles.sectionWrap}>
+            <SectionHeader
+                title="Ge In Touch"
+                subtitle="Contact me via any of the list below"
+            />
+
+            <div className={`container ${styles.contactWrapper}`}>
+                <a href="#" className={styles.contactOption}>
+                    <MdOutlineEmail className={styles.contactIcon} />
+                </a>
+                <a href="#" className={styles.contactOption}>
+                    <RiMessengerLine className={styles.contactIcon} />
+                </a>
+                <a href="#" className={styles.contactOption}>
+                    <BsWhatsapp className={styles.contactIcon} />
+                </a>
+            </div>
+        </section>
     );
 }
 
